1e903381843ada3f197dda63ed348873977affbd
Ravjodh Heer <ravjodhheer@Ravjodhs-MacBook-Pro.local>
2022-11-09 22:58:49 -0800

    first commit

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Sreerag Rajan',
+    title: "Web Developer",
+    desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
+    image: 'https://media-exp1.licdn.com/dms/image/C5603AQH-tv1TQJmIdA/profile-displayphoto-shrink_400_400/0/1660676811703?e=1673481600&v=beta&t=mm_O-D--WDOgGTrKzlL71sgz0rdML6G1xauERXbygBM',
+    resumePdf: resume
+}
