24668e98462d1b613d9727a7840a8b2f822591f1
ravjodhheer <Ravjodhheer@gmail.com>
2023-01-16 20:59:29 -0800

    fixed landing image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import front_image from '../assets/png/Ravjodh_Medium.png'
 
 export const headerData = {
     name: 'Ravjodh Singh Heer',
     title: "Software Engineer. FullStack Developer. Cloud Specialist.",
     desciption:"Self-taught programmer with a passion for creativity and development. Incoming software engineer and student at UC Santa Cruz. Feel free to look around and learn more about me on this interactive web portfolio!",
-    image: 'https://media-exp1.licdn.com/dms/image/C5603AQH-tv1TQJmIdA/profile-displayphoto-shrink_400_400/0/1660676811703?e=1673481600&v=beta&t=mm_O-D--WDOgGTrKzlL71sgz0rdML6G1xauERXbygBM',
+    image: front_image,
     resumePdf: resume
 }
