24535f8fef2205d0ae112bab7f6547642c5da5ca
ravjodhheer <Ravjodhheer@gmail.com>
2023-10-23 21:07:39 -0700

    about data and skills changes

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "About Me",
-    description1: "Hello! I'm Ravjodh Heer, I am a Senior pursuing Computer Science at UC Santa Cruz.",
+    description1: "Hello! I'm Ravjodh Heer, I am a Computer Science graduate from UC Santa Cruz.",
     description2: "I'm a self-taught programmer that specializes in full-stack web development and cloud applications. I love to fuel my innovation by exploring creative ideas and executing them. I've always been fascinated with the idea of how things worked - which in addition to my love of computers and digital media, led me to where I am today and kickstarted my software development passion.",
-    description3: "Outside of work, I enjoy tinkering with computer hardware, playing video games, and messing with cars. These hobbies accelerate some of my creativity and trickle into the inspiration for some of my future project ideas!",
+    description3: "Outside of work, I enjoy working hands-on with computer hardware, playing video games, and working on cars. These hobbies accelerate some of my creativity and trickle into the inspiration for some of my future project ideas!",
     image: 2
 }
