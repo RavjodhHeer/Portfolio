9e4616bc3ccd3567f6ad72e257ac1771c2359cf0
RavjodhHeer <Ravjodhheer@gmail.com>
2023-12-11 02:09:39 -0800

    updated resume and header data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import front_image from '../assets/png/Ravjodh_Medium.png'
 export const headerData = {
     name: 'Ravjodh Heer',
     title: "Software Engineer. FullStack Developer. Cloud Specialist.",
-    desciption:"Self-taught programmer with a passion for creativity and development. Incoming software engineer and student at UC Santa Cruz. Feel free to look around and learn more about me on this interactive web portfolio!",
+    desciption:"Self-taught programmer with a passion for creativity and development. AWS Engineer and UC Santa Cruz graduate. Feel free to look around and learn more about me on this interactive web portfolio!",
     image: front_image,
     resumePdf: resume
 }
